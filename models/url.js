f11dac72439b434ddbfe769ffa6a67a22877d8c2
curious33 <curious.startpro@gmail.com>
2022-09-06 19:01:39 +0500

    add: url model

---
+++ models/url.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    generated_url: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const URL = mongoose.model("url", urlSchema);
+
+module.exports = URL;
