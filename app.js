98e4a6f058ecdbfdde7fe4418618c7f23b25fdf0
curious33 <curious.startpro@gmail.com>
2022-09-07 16:29:28 +0500

    fix: minor bugs

--- app.js
+++ app.js
@@ -2,27 +2,82 @@ const express = require("express");
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
+const mongoose = require("mongoose");
+
+const Console = require("./helpers/console");
+const urlGenerator = require("./helpers/urlGenerator");
+const URLSchema = require("./models/url");
 
 const app = express();
 const PORT = 8080;
 
+urlGenerator("hello");
+
+const dbURI =
+  "mongodb+srv://curious:cush@cluster0.winwmn8.mongodb.net/cush?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI)
+  .then((res) => {
+    app.listen(PORT, (err) => {
+      if (err) Console.errorLogger(err);
+      console.log("Server's listening port:", PORT);
+    });
+  })
+  .catch((err) => Console.errorLogger(err));
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
 
 const logPath = path.join(__dirname, "logs", "request.log");
 const accessLogStream = fs.createWriteStream(logPath, { flags: "a" });
 app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
-app.post("/shorten", (req, res) => {
-  const newUrl = req.body;
-  console.log("post request", newUrl);
-  res.redirect("/");
+app.post("/", (req, res) => {
+  const url = req.body.url;
+  if (url) {
+    URLSchema.find()
+      .then((result) => {
+        const existingUrls = result.map((obj) => obj.generated_url);
+        const generateUrl = urlGenerator(url, existingUrls);
+        const newGeneratedUrl = new URLSchema({
+          url: req.body.url,
+          generated_url: generateUrl,
+        });
+
+        if (generateUrl) {
+          newGeneratedUrl
+            .save()
+            .then((result) => res.json({ generated_url: result.generated_url }))
+            .catch((err) =>
+              res.status(500).json({ error: "Something went wrong" })
+            );
+        }
+      })
+      .catch((err) => Console.errorLogger(err, req));
+  }
 });
 
-app.listen(PORT, () => {
-  console.log("Server's listening port:", PORT);
+app.get("/redirect/:id", (req, res) => {
+  const id = req.params.id;
+
+  URLSchema.findOne({ generated_url: id })
+    .then((result) => {
+      if (result) {
+        res.redirect(result.url);
+      }
+
+      res.redirect("/");
+    })
+    .catch((err) => (err) => Console.errorLogger(err, req));
+});
+
+app.use((req, res) => {
+  res.status(404).render("404");
 });
