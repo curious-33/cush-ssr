c25e4481ca37dc816597d19128722f8468ccf080
curious-33 <curious.startpro@gmail.com>
2023-01-28 01:56:01 +0500

    feat: add env config

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const mongoose = require("mongoose");
 const Console = require("./helpers/console");
 const urlShortenRoutes = require("./routes/shorten");
 const urlShortenApiRoutes = require("./routes/api");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const dbURI =
-  "mongodb+srv://curious:cush@cluster0.winwmn8.mongodb.net/cush?retryWrites=true&w=majority";
+const dbURI = `mongodb://${process.env.MONGODB_HOST}/${process.env.MONGODB_DBNAME}`;
 
 mongoose
-  .connect(dbURI)
-  .then((res) => {
-    app.listen(PORT, (err) => {
-      if (err) Console.errorLogger(err);
-      console.log("Server's listening port:", PORT);
-    });
-  })
-  .catch((err) => Console.errorLogger(err));
+	.connect(dbURI)
+	.then((res) => {
+		app.listen(PORT, (err) => {
+			if (err) Console.errorLogger(err);
+			console.log("Server's listening port:", PORT);
+		});
+	})
+	.catch((err) => Console.errorLogger(err));
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -29,11 +29,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 if (fs.existsSync("./logs")) {
-  const logPath = path.join(__dirname, "logs", "request.log");
-  const accessLogStream = fs.createWriteStream(logPath, { flags: "a" });
-  app.use(morgan("combined", { stream: accessLogStream }));
+	const logPath = path.join(__dirname, "logs", "request.log");
+	const accessLogStream = fs.createWriteStream(logPath, { flags: "a" });
+	app.use(morgan("combined", { stream: accessLogStream }));
 } else {
-  fs.mkdirSync("logs", { recursive: true });
+	fs.mkdirSync("logs", { recursive: true });
 }
 
 // console.log(require("luxon").DateTime.local())
@@ -41,5 +41,5 @@ app.use(urlShortenRoutes);
 app.use("/api/v1/shorten", urlShortenApiRoutes);
 
 app.use((req, res) => {
-  res.status(404).render("404");
+	res.status(404).render("404");
 });
